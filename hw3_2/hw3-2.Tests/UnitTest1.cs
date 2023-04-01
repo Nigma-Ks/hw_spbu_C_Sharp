4aad5c4d22c6031c2c08500075b3ac359cef88e4
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2023-04-01 22:22:33 +0300

    Add first solution3-2

---
+++ hw3_2/hw3-2.Tests/UnitTest1.cs
@@ -0,0 +1,85 @@
+namespace hw3_2.Tests;
+
+public class LzwTests
+{
+    private Lzw lzw;
+
+    [SetUp]
+    public void Initialize()
+    {
+        lzw = new Lzw();
+    }
+
+    [Test]
+    public void EmptyStringCompression()
+    {
+        bool isEmpty;
+        List<Byte[]> result = lzw.LzwCompression("", out isEmpty);
+        Assert.IsTrue(isEmpty);
+    }
+
+    [Test]
+    public void OneCharStringCompression()
+    {
+        bool isEmpty;
+        List<Byte[]> result = lzw.LzwCompression("\n", out isEmpty);
+        Assert.IsTrue(!isEmpty && BitConverter.ToUInt32(result[0]) == (int)'\n');
+    }
+
+    [Test]
+    public void StringCompression()
+    {
+        bool isEmpty;
+        UInt32[] arrayOfRightIntCodes = { 97, 98, 97, 99, 256, 97, 100, 260, 259, 257, 101 };
+        List<Byte[]> result = lzw.LzwCompression("abacabadabacabae", out isEmpty);
+        int resultLenght = result.Count;
+        if (resultLenght != 11 || isEmpty) //Lenght of arrayOfRightCodes
+        {
+            Assert.IsTrue(false);
+        }
+
+        for (int i = 0; i < resultLenght; i++)
+        {
+            if (BitConverter.ToUInt32(result[i]) != arrayOfRightIntCodes[i])
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public void StringWithRepeatedSymbolsCompression()
+    {
+        bool isEmpty;
+        UInt32[] arrayOfRightIntCodes = { 97, 256, 257, 258 };
+        List<Byte[]> result = lzw.LzwCompression("aaaaaaaaaa", out isEmpty);
+        int resultLenght = result.Count;
+        if (resultLenght != 4 || isEmpty) //Lenght of arrayOfRightCodes
+        {
+            Assert.IsTrue(false);
+        }
+
+        for (int i = 0; i < resultLenght; i++)
+        {
+            if (BitConverter.ToUInt32(result[i]) != arrayOfRightIntCodes[i])
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public void StringCompressionPlusDecompression()
+    {
+        bool isEmpty;
+        bool isCorrect;
+        string testString = "\n\n\n\\pbdbdbtjjj\\{{";
+        List<Byte[]> resultOfCompression = lzw.LzwCompression(testString, out isEmpty);
+        string resultOfDecompression = lzw.LzwDecompression(resultOfCompression, out isEmpty, out isCorrect);
+        Assert.IsTrue(resultOfDecompression == testString);
+    }
+}
