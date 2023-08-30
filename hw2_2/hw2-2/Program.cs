1127d1b6d4a822c47eecc29215be4e8dfa457450
Ksenia Nigmatulina <kv.nigma@gmail.com>
2023-08-30 17:10:27 +0300

    1. Классы в разных файлах 2. string -> string? 3. возвращение не через параметр, сразу парой 4. убрала копипасту в калькуляторе

--- hw2_2/hw2-2/Program.cs
+++ hw2_2/hw2-2/Program.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+
 
 namespace hw2_2
 {
@@ -8,13 +7,12 @@ namespace hw2_2
         static void Main(string[] args)
         {
             StackCalculatorOfReversePolishNotation calculator = new();
-            bool correctNotation;
             Console.WriteLine(
                 "This program calculates expression in reverse polish notation by stack, you can choose one of the options:\n" +
                 "1. Stack on array base\n" +
                 "2. Stack on list base\n");
             Console.Write("write number 1 or 2: ");
-            string choice = Console.ReadLine();
+            string? choice = Console.ReadLine();
             while (choice != "1" && choice != "2")
             {
                 Console.WriteLine("You entered something wrong!");
@@ -26,7 +24,7 @@ namespace hw2_2
             }
 
             Console.Write("Enter expression in reverse polish notation: ");
-            string expression = Console.ReadLine();
+            string? expression = Console.ReadLine();
             if (expression == null)
             {
                 Console.Write("No string! Enter expression in reverse polish notation: ");
@@ -36,8 +34,8 @@ namespace hw2_2
             if (choice == "1")
             {
                 FloatStackBasedOnArray stack = new();
-                float result = calculator.ToCalculate(expression, stack, out correctNotation);
-                if (correctNotation)
+                float? result = calculator.ToCalculate(expression, stack);
+                if (result != null)
                 {
                     Console.WriteLine($"Result: {result}");
                     return;
@@ -48,8 +46,8 @@ namespace hw2_2
             else
             {
                 FloatStackBasedOnList stack = new();
-                float result = calculator.ToCalculate(expression, stack, out correctNotation);
-                if (correctNotation)
+                float? result = calculator.ToCalculate(expression, stack);
+                if (result != null)
                 {
                     Console.WriteLine($"Result: {result}");
                     return;
@@ -60,239 +58,4 @@ namespace hw2_2
         }
     }
 
-    public class FloatStackBasedOnArray : IStack
-    {
-        private const int defaultArraySize = 20;
-
-        private float[] _ArrayOfOperands;
-
-        private int size = 0;
-
-        public int Size { get; private set; }
-
-        public FloatStackBasedOnArray()
-        {
-            _ArrayOfOperands = new float[defaultArraySize];
-        }
-
-        private float[] ExpandArrayOnDefaultSize(float[] arrayToExpand)
-        {
-            float[] newExpandedArray = new float[arrayToExpand.Length + defaultArraySize];
-            for (int i = 0; i < arrayToExpand.Length; i++)
-            {
-                newExpandedArray[i] = arrayToExpand[i];
-            }
-
-            return newExpandedArray;
-        }
-
-        public void Add(float value)
-        {
-            if (Size == _ArrayOfOperands.Length)
-            {
-                _ArrayOfOperands = ExpandArrayOnDefaultSize(_ArrayOfOperands);
-            }
-
-            _ArrayOfOperands[Size] = value;
-            Size = ++Size;
-        }
-
-        public float Remove(out bool isEmpty)
-        {
-            isEmpty = false;
-            if (Size < 1)
-            {
-                isEmpty = true;
-                return 0;
-            }
-
-            float value = _ArrayOfOperands[Size - 1]; //get head of the stack
-            _ArrayOfOperands[Size - 1] = 0;
-            Size--;
-            return value;
-        }
-    }
-
-    public class FloatStackBasedOnList : IStack
-    {
-        private List<float> _listOfOperands;
-
-        private int size = 0;
-
-        public int Size { get; private set; }
-
-        public FloatStackBasedOnList()
-        {
-            _listOfOperands = new List<float>();
-        }
-
-        public void Add(float value)
-        {
-            _listOfOperands.Add(value);
-            Size = ++Size;
-        }
-
-        public float Remove(out bool isEmpty)
-        {
-            isEmpty = false;
-            if (Size < 1)
-            {
-                isEmpty = true;
-                return 0;
-            }
-
-            float value = _listOfOperands[Size - 1]; //get head of the stack
-            _listOfOperands.RemoveAt(Size - 1);
-            Size--;
-            return value;
-        }
-    }
-
-
-    public interface IStack
-    {
-        void Add(float value);
-
-        float Remove(out bool isEmpty);
-    }
-
-    public class StackCalculatorOfReversePolishNotation
-    {
-        public float delta = 0.0001f; //null comparison precision
-
-        private string[] ToParse(string expressionInReversePolishNotation)
-        {
-            return expressionInReversePolishNotation.Split(new char[] { ' ' },
-                StringSplitOptions.RemoveEmptyEntries); //if there more than one spaces
-        }
-
-        public float ToCalculate(string expessionInReversePolishNotation, IStack stack,
-            out bool isCorrectExpression)
-        {
-            isCorrectExpression = true;
-            bool stackOver;
-            if (expessionInReversePolishNotation == null)
-            {
-                isCorrectExpression = false;
-                return 0;
-            }
-            string[] parsedExpression = ToParse(expessionInReversePolishNotation);
-            float result;
-            for (int i = 0; i < parsedExpression.Length; i++)
-            {
-                int currOperand;
-                bool isOperand = int.TryParse(parsedExpression[i], out currOperand);
-                if (isOperand)
-                {
-                    stack.Add((float)currOperand);
-                }
-                else
-                {
-                    if (parsedExpression[i] == "+")
-                    {
-                        bool isEmpty;
-                        float fstNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        float sndNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        stack.Add(fstNumber + sndNumber);
-                    }
-                    else if (parsedExpression[i] == "/")
-                    {
-                        bool isEmpty;
-                        float fstNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        float sndNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        if (Math.Abs(fstNumber) < delta)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        stack.Add(sndNumber / fstNumber);
-                    }
-                    else if (parsedExpression[i] == "-")
-                    {
-                        bool isEmpty;
-                        float fstNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        float sndNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        stack.Add(sndNumber - fstNumber);
-                    }
-                    else if (parsedExpression[i] == "*")
-                    {
-                        bool isEmpty;
-                        float fstNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        float sndNumber = stack.Remove(out isEmpty);
-                        if (isEmpty)
-                        {
-                            isCorrectExpression = false;
-                            return 0;
-                        }
-
-                        stack.Add(sndNumber * fstNumber);
-                    }
-                    else
-                    {
-                        isCorrectExpression = false;
-                        return 0;
-                    }
-                }
-            }
-
-            result = stack.Remove(out stackOver);
-            if (stackOver) //in stack was nothing
-            {
-                isCorrectExpression = false;
-                return 0;
-            }
-
-            stack.Remove(out stackOver);
-            if (stackOver)
-            {
-                return result; //in stack was only result
-            }
-
-            isCorrectExpression = false;
-            return 0;
-        }
-    }
 }
