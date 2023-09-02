422e62b7f0f8289db408b5469191b4eef2001ebe
Ksenia Nigmatulina <kv.nigma@gmail.com>
2023-09-02 12:17:14 +0300

    Класс ошибок больше не пустой

--- hw4_1/hw4_1/ExceptionClasses.cs
+++ hw4_1/hw4_1/ExceptionClasses.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿
 namespace hw4_1
 {
-	public class ExceptionClasses
-	{
-		public ExceptionClasses()
-		{
-		}
-	}
+    public class WrongSymbolsInExpressionException : ArgumentException
+    {
+        public WrongSymbolsInExpressionException()
+        {
+        }
+
+        public WrongSymbolsInExpressionException(string message) : base(message)
+        {
+        }
+
+        public WrongSymbolsInExpressionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+
+    public class IncorrectExpressionException : ArgumentException
+    {
+        public IncorrectExpressionException()
+        {
+        }
+
+        public IncorrectExpressionException(string message) : base(message)
+        {
+        }
+
+        public IncorrectExpressionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+    public class IncorrectOperationException : ArgumentException
+    {
+        public IncorrectOperationException()
+        {
+        }
+
+        public IncorrectOperationException(string message) : base(message)
+        {
+        }
+
+        public IncorrectOperationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
 }
 
