fe9b0b8dd08a8b8981309a20fbd07212c9068c0d
Ksenia Nigmatulina <kv.nigma@gmail.com>
2023-09-03 12:00:59 +0300

    Решение без тестов и карты

---
+++ hw6-2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace hw6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            //string path = "//Users//nigmatulinakseniya//Downloads//Map.txt";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Incorrect file path!");
+                return;
+            }
+
+            Map map = new(File.ReadAllLines(path));
+
+            var eventLoop = new EventLoop();
+            var game = new Game(map);
+
+            eventLoop.LeftHandler += game.OnLeft;
+            eventLoop.RightHandler += game.OnRight;
+            eventLoop.UpHandler += game.OnTop;
+            eventLoop.DownHandler += game.OnBottom;
+            eventLoop.StartGame += game.Start;
+            eventLoop.Run();
+        }
+    }
+}
